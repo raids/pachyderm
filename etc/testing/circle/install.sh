e082a6ac1eb4369f4955b2226766d6f01a78c954
Brendon Carroll <7072757+brendoncarroll@users.noreply.github.com>
2021-07-22 11:00:50 -0400

    revert go update

--- etc/testing/circle/install.sh
+++ etc/testing/circle/install.sh
@@ -17,11 +17,6 @@ sudo apt-get install -y -qq \
   pv \
   shellcheck
 
-## Install Go
-wget https://dl.google.com/go/go1.16.6.linux-amd64.tar.gz 
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf go1.16.6.linux-amd64.tar.gz
-
 # Install fuse
 sudo modprobe fuse
 sudo chmod 666 /dev/fuse
